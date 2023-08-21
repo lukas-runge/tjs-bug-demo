5f3de743f08f3ca838659fa701353fcb1d344c66
Lukas Runge <login@lukasrunge.de>
2023-08-21 19:00:24 +0200

    🐛✨ found workaround

--- main.ts
+++ main.ts
@@ -2,8 +2,10 @@ import * as TJS from "typescript-json-schema";
 
 const settings: TJS.PartialArgs = { required: true };
 
-const program = TJS.getProgramFromFiles(["src/udr.next.d.ts"]);
+const files = ["src/udr.next.d.ts"];
 
-const schema = TJS.buildGenerator(program, settings)?.getSchemaForSymbol("Device")
+const program = TJS.getProgramFromFiles(files);
+
+const schema = TJS.buildGenerator(program, settings, files)?.getSchemaForSymbol("Device")
 
 console.log(JSON.stringify(schema, null, 4));
